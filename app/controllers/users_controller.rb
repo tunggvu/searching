c1591e871c3c2ad57ef3a174afb5b73372aefd07
tunggvu <vutungtn95@gmail.com>
2017-08-16 09:58:29 +0700

    second

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
     @users = params[:term].present? ? User.search("name: #{params[:term]}") : User.search("id: 1")
+    @count = @users.count
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10000.times do
-  User.create name: "#{Faker::Name.name} add more"
+38613674.times do
+  User.create name: "#{Faker::Name.name}"
 end
